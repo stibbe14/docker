009f80596bec0c647d21f442a726b37a254e8854
stibbe14 <1208882@student.roc-nijmegen.nl>
2024-03-01 12:39:01 +0100

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>The Red Cafe</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="all">
+    <div class="infobalk">
+        <div class="logo">
+            <img src="assets/logo.png" alt="">
+        </div>
+    <div class="parent">
+        <div class="menubalk">
+            <a href="#">Home</a>
+            <a href="#">Menu</a>
+            <a href="#">Bezorging</a>
+            <a href="#">Contact</a>
+        </div>
+        <div class="login">
+
+        </div>
+        <div class="order">
+
+        </div>
+
+    </div>
+</div>
+        <div class="middle">
+            <div class="textarea">
+                <div class="headtext">
+                    <h1>Lorem ipsum dolor sit amet</h1>
+                </div>
+                <div class="littletext">
+                <h3>consectetur adipiscing elit, sed do eiusmod eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proidenttempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore. </h3>
+                </div>
+                <div class="shopbutton">
+                    <div class="shopbtn">
+                       <a href="#">Bestel nu</a>
+                    </div>
+                </div>
+            </div>
+            <div class="imagearea">
+                <img src="assets/food.png" alt="">
+            </div>
+        </div>
+
+<footer>
+<div class="text1">
+<p>
+    Heyendaalseweg 98,
+    
+    Nijmegen 6525EE
+</p>
+</div>
+
+<div class="text2">
+<p>
+Heyendaalseweg 98,
+    
+    Nijmegen 6525EE
+</p>
+</div>    
+
+</footer>
+       
+    
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
+</html>
